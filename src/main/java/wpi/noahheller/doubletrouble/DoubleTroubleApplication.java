d56656f4aec323086c91a641ff6d745f3302e391
Noah Heller <dev.noahheller@pm.me>
2026-03-21 16:22:53 -0400

    Updated naming and pom

--- src/main/java/wpi/noahheller/doubletrouble/DoubleTroubleApplication.java
+++ src/main/java/wpi/noahheller/doubletrouble/DoubleTroubleApplication.java
@@ -7,11 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class DoubleTroubleApplication extends Application {
     DoubleTroubleController controller;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(DoubleTroubleApplication.class.getResource("double-trouble.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 640, 320);
         controller = fxmlLoader.getController();
         stage.setTitle("Double Trouble");
